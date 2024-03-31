1231ac0b4d53c37d23064f5664ab3dcb0161ba16
feitosadanilo <165584720+feitosadanilo@users.noreply.github.com>
2024-03-31 16:18:27 -0300

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-normal italic">
+      <h1 className="text-6xl font-extralight uppercase">
+        Danilo
+        <span className="font-extrabold">Feitosa</span>
       </h1>
+      <div className="flex flex-row gap-8">
+        <Link href={"https://linkedin.com/in/danilofeitosa"} target="_blank">
+          <Image src={"/linkedin.png"} alt={"LinkedIn"} width={36} height={36} />
+        </Link>
+        <Link href={"https://github.com/feitosadanilo"} target="_blank">
+          <Image src={"/github.png"} alt={"GitHub"} width={36} height={36} />
+        </Link>
+      </div>
     </main>
   );
 }
